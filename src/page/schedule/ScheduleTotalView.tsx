9bbeedfa2ed3a156758a3db70e303c7bb5edcd71
mingmeng030 <djwls_0716@naver.com>
2023-01-21 23:53:50 +0900

    add scheduleTotal

--- src/page/schedule/ScheduleTotalView.tsx
+++ src/page/schedule/ScheduleTotalView.tsx
@@ -2,65 +2,167 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ScheduleTotalView = () => {
-  const scheduleWeekly = [
-    {
-      day: "월요일",
-      date: "01/02",
-      schedule: [
-        { name: "김어진", time: "10:00-14:00", backgroundColor: "#ffd6a5" },
-        { name: "이예빈", time: "13:00-18:00", backgroundColor: "#ffadad" },
-      ],
-    },
-    {
-      day: "화요일",
-      date: "01/03",
-      schedule: [
-        { name: "김어진", time: "10:00-14:00", backgroundColor: "#ffd6a5" },
-        { name: "이예빈", time: "13:00-18:00", backgroundColor: "#ffadad" },
-      ],
-    },
-    {
-      day: "수요일",
-      date: "01/04",
-      schedule: [
-        { name: "김어진", time: "10:00-14:00", backgroundColor: "#ffd6a5" },
-      ],
-    },
-    {
-      day: "목요일",
-      date: "01/05",
-      schedule: [
-        { name: "고구마", time: "10:00-14:00", backgroundColor: "#fdffb6" },
-        { name: "이예빈", time: "13:00-18:00", backgroundColor: "#ffadad" },
-      ],
-    },
-    {
-      day: "금요일",
-      date: "01/06",
-      schedule: [
-        { name: "옥수수", time: "10:00-14:00", backgroundColor: "a0c4ff" },
-        { name: "감자밭", time: "13:00-18:00", backgroundColor: "bdb2ff" },
-      ],
-    },
-    {
-      day: "토요일",
-      date: "01/07",
-      schedule: [
-        { name: "감자밭", time: "10:00-14:00", backgroundColor: "#a0c4ff" },
-        { name: "고구마", time: "13:00-18:00", backgroundColor: "#fdffb6" },
-      ],
-    },
-    {
-      day: "일요일",
-      date: "01/08",
-      schedule: [
-        { name: "고구마", time: "10:00-14:00", backgroundColor: "#fdffb6" },
-        { name: "이예빈", time: "13:00-18:00", backgroundColor: "#ffadad" },
-      ],
-    },
-  ];
+  const scheduleTotal = {
+    totalList: [
+      {
+        day: "월",
+        date: "01/02",
+        name: "김어진",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+        backgroundColor: "#ffd6a5",
+      },
+      {
+        day: "월",
+        date: "01/02",
+        name: "이예빈",
+        time: "13:00-18:00",
+        totalWorkTime: "5",
+        backgroundColor: "#ffadad",
+      },
+
+      {
+        day: "화",
+        date: "01/03",
+        name: "김어진",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+
+        backgroundColor: "#ffd6a5",
+      },
+      {
+        day: "화",
+        date: "01/03",
+        name: "이예빈",
+        time: "13:00-18:00",
+        totalWorkTime: "5",
+        backgroundColor: "#ffadad",
+      },
+
+      {
+        day: "수",
+        date: "01/04",
+        name: "김어진",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+        backgroundColor: "#ffd6a5",
+      },
+
+      {
+        day: "목",
+        date: "01/05",
+        name: "고구마",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+        backgroundColor: "#fdffb6",
+      },
+      {
+        day: "목",
+        date: "01/05",
+        name: "이예빈",
+        time: "13:00-18:00",
+        totalWorkTime: "5",
+        backgroundColor: "#ffadad",
+      },
+
+      {
+        day: "금",
+        date: "01/06",
+        name: "옥수수",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+        backgroundColor: "a0c4ff",
+      },
+      {
+        day: "금",
+        date: "01/06",
+        name: "감자밭",
+        time: "13:00-18:00",
+        totalWorkTime: "5",
+        backgroundColor: "bdb2ff",
+      },
+
+      {
+        day: "토",
+        date: "01/07",
+        name: "감자밭",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+        backgroundColor: "#a0c4ff",
+      },
+      {
+        day: "토",
+        date: "01/07",
+        name: "고구마",
+        time: "13:00-18:00",
+        totalWorkTime: "5",
+        backgroundColor: "#fdffb6",
+      },
+
+      {
+        day: "일",
+        date: "01/08",
+        name: "고구마",
+        time: "10:00-14:00",
+        totalWorkTime: "4",
+        backgroundColor: "#fdffb6",
+      },
+      {
+        day: "일",
+        date: "01/08",
+        name: "이예빈",
+        time: "13:00-18:00",
+        totalWorkTime: "5",
+        backgroundColor: "#ffadad",
+      },
+    ],
+    employee: [
+      { name: "김어진", backgroundColor: "#ffd6a5" },
+      { name: "이예빈", backgroundColor: "#ffadad" },
+      { name: "감자밭", backgroundColor: "#a0c4ff" },
+      { name: "고구마", backgroundColor: "#fdffb6" },
+      { name: "옥수수", backgroundColor: "#a0c4ff" },
+    ],
+  };
   return (
-    <div className="ScheduleTotalView-top-container">ScheduleTotalView</div>
+    <div className="ScheduleTotalView-top-container">
+      <div className="ScheduleTotalView-left-container">
+        <ul className="employee-list-ul">
+          {scheduleTotal.employee.map((item) => (
+            <li className="employee-list-li">
+              <span
+                className="employee-list-li-color"
+                style={{ backgroundColor: item.backgroundColor }}
+              >
+                &nbsp;&nbsp;
+              </span>
+              <span className="employee-list-li-name"> {item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="ScheduleTotalView-rigt-container">
+        <ul className="totalList-ul">
+          {scheduleTotal.totalList.map((item) => (
+            <li className="totalList-li">
+              <span className="totalList-li-date">
+                {item.date}({item.day})
+              </span>
+              <span
+                className="totalList-li-name"
+                style={{ backgroundColor: item.backgroundColor }}
+              >
+                {item.name}
+              </span>
+              <span className="totalList-li-time">{item.time}</span>
+              <span className="totalList-li-totalWorkTime">
+                {item.totalWorkTime}시간
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 export default ScheduleTotalView;
