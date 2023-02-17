da7d8ecbb2e81fbc12c6cc52f583b557d61c73e1
mingmeng030 <djwls_0716@naver.com>
2023-02-17 16:54:39 +0900

    update files

--- src/page/User/Mypage.tsx
+++ src/page/User/Mypage.tsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import MypageView from "./MypageView";
 import "./style/mypage.scss";
-import { useLocation, useNavigate } from "react-router-dom";
 import * as type from "./type";
 import { GetUserInfoApi } from "../../api/mypage";
 
 const Mypage = () => {
-  const navigate = useNavigate();
-
   const [userInfo, setUserInfo] = useState<type.userInfoProps>({
     email: "",
     storeName: "",
--- src/page/User/UpdateUser.tsx
+++ src/page/User/UpdateUser.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import UpdateUserView from "./UpdateUserView";
 import "./style/mypage.scss";
 import { useLocation, useNavigate } from "react-router-dom";
