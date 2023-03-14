58d473af27d75c9accf18acfa58fd91692e0c2fb
mingmeng030 <djwls_0716@naver.com>
2023-03-14 15:07:01 +0900

    update statistics

--- src/page/statistics/StatisticsView.tsx
+++ src/page/statistics/StatisticsView.tsx
@@ -27,86 +27,94 @@ const StatisticsView = ({
   chartRef,
   statisticsData,
 }: type.statisticsViewProps) => {
-  let barChart: ChartJS | undefined;
-  const ctx = chartRef.current?.getContext("2d");
+  let barChart: ChartJS;
 
-  if (barChart?.ctx == undefined) {
-    if (ctx) {
-      console.log("create" + statisticsData);
-      barChart = new ChartJS(ctx, {
-        type: "bar",
-        data: statisticsData
-          ? statisticsData
-          : {
-              labels: [],
-              datasets: [
-                {
-                  axis: "y",
-                  data: [],
-                  backgroundColor: [],
-                  borderRadius: Number.MAX_VALUE,
-                  maxBarThickness: 20,
-                  borderSkipped: false,
-                },
-              ],
-            },
-        options: {
-          responsive: false,
-          plugins: {
-            legend: {
-              display: false,
-            },
-          },
-          indexAxis: "y",
-          scales: {
-            x: {
-              ticks: {
-                font: {
-                  size: 18,
-                },
-              },
-              grid: {
-                display: false,
-              },
-              border: {
-                display: false,
-              },
-            },
-            y: {
-              ticks: {
-                font: {
-                  size: 18,
-                },
-                color: "black",
-              },
-              grid: {
-                display: false,
-              },
-              border: {
-                display: false,
-              },
-            },
-          },
-        },
-      });
-      console.log(barChart);
-    }
-  } else if (barChart && barChart.id.toString() == "0") {
-    console.log("chart destroyed");
-    barChart.clear();
-    barChart.destroy();
-  }
   useEffect(() => {
-    console.log(statisticsData);
-    console.log(barChart);
-
-    if (statisticsData && barChart) {
+    if (barChart && statisticsData) {
+      console.log("chart updated");
+      // barChart.clear();
+      // barChart.destroy();
       barChart.data.labels = statisticsData.labels;
       barChart.data.datasets[0] = statisticsData.datasets[0];
       barChart.update();
       console.log("updated");
+    } else {
+      const ctx = chartRef.current?.getContext("2d");
+
+      if (ctx) {
+        console.log("create" + statisticsData);
+        barChart = new ChartJS(ctx, {
+          type: "bar",
+          data: statisticsData
+            ? statisticsData
+            : {
+                labels: [],
+                datasets: [
+                  {
+                    axis: "y",
+                    data: [],
+                    backgroundColor: [],
+                    borderRadius: Number.MAX_VALUE,
+                    maxBarThickness: 20,
+                    borderSkipped: false,
+                  },
+                ],
+              },
+          options: {
+            responsive: false,
+            plugins: {
+              legend: {
+                display: false,
+              },
+            },
+            indexAxis: "y",
+            scales: {
+              x: {
+                ticks: {
+                  font: {
+                    size: 18,
+                  },
+                },
+                grid: {
+                  display: false,
+                },
+                border: {
+                  display: false,
+                },
+              },
+              y: {
+                ticks: {
+                  font: {
+                    size: 18,
+                  },
+                  color: "black",
+                },
+                grid: {
+                  display: false,
+                },
+                border: {
+                  display: false,
+                },
+              },
+            },
+          },
+        });
+        console.log(barChart);
+      }
     }
-  }, [statisticsData]);
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(statisticsData);
+  //   console.log(barChart);
+
+  //   if (statisticsData && barChart) {
+  //     barChart.data.labels = statisticsData.labels;
+  //     barChart.data.datasets[0] = statisticsData.datasets[0];
+  //     barChart.update();
+  //     console.log("updated");
+  //   }
+  // }, [statisticsData]);
 
   const chartHeight = statisticsList ? statisticsList.length * 100 : 600;
 
