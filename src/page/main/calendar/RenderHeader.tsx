021a446eb9d16f42f44c86edeb362da0de1ea0f7
mingmeng030 <djwls_0716@naver.com>
2023-03-20 14:58:49 +0900

    update schedule

--- src/page/main/calendar/RenderHeader.tsx
+++ src/page/main/calendar/RenderHeader.tsx
@@ -10,28 +10,26 @@ const RenderHeader = ({
   onCreateMemoClick,
 }: type.renderheaderProps) => {
   return (
-    <div className="CalendarView-container">
-      <div className="header row">
-        <div className="col col-start">
-          <h2 className="text">
-            {format(currentMonth, "yyyy")}년&nbsp;
-            <span className="text month">{format(currentMonth, "M")}월</span>
-          </h2>
-        </div>
+    <div className="header row">
+      <div className="col col-start">
+        <h2 className="text">
+          {format(currentMonth, "yyyy")}년&nbsp;
+          <span className="text month">{format(currentMonth, "M")}월</span>
+        </h2>
+      </div>
 
-        <div className="col col-end">
-          <button
-            className="createWorkcheckButton"
-            onClick={onCreateWorkcheckClick}
-          >
-            출근부 작성하기
-          </button>
-          <button className="createMemoButton" onClick={onCreateMemoClick}>
-            메모 작성하기
-          </button>
-          <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
-          <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
-        </div>
+      <div className="col col-end">
+        <button
+          className="createWorkcheckButton"
+          onClick={onCreateWorkcheckClick}
+        >
+          출근부 작성하기
+        </button>
+        <button className="createMemoButton" onClick={onCreateMemoClick}>
+          메모 작성하기
+        </button>
+        <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
+        <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
       </div>
     </div>
   );
