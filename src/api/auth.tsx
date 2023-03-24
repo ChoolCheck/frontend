d7033d518420a7c8b2dd5f427096310a83e695fc
mingmeng030 <djwls_0716@naver.com>
2023-03-24 19:49:57 +0900

    update email code

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -62,7 +62,7 @@ export async function CertificateEmailApi({
 }: type.emailProps) {
   await axios({
     method: "POST",
-    url: `${config.api}/user/email?=${email}`,
+    url: `${config.api}/user/email?email=${email}`,
     headers: {
       "Content-Type": `application/json`,
     },
