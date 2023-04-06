7a4af08d7414fed37569e6bc5db307fe13fbc604
mingmeng030 <djwls_0716@naver.com>
2023-04-06 20:36:07 +0900

    update config

--- src/static/config.tsx
+++ src/static/config.tsx
@@ -1,4 +1,4 @@
 export const config = {
-  api: "http://15.164.3.169:8080",
-  client: "http://choolcheck-frontend.s3-website.ap-northeast-2.amazonaws.com",
+  api: "http://3.38.254.154:8080",
+  client: "http://choolcheck-front.s3-website.ap-northeast-2.amazonaws.com",
 };
